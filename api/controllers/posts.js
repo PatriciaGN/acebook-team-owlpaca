1a003b328291efe121536c7bccc1487fe9dbb108
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-09 17:28:28 +0000

    Add .json() to res.status(204) to fix hang

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -27,33 +27,18 @@ const PostsController = {
     let findAndUpdate = async (id) => {
       const filter = { _id: id };
       console.log(filter);
+      console.log(id);
+
       const update = { $inc: { agrees: 1 } };
-      let post = Post.findOneAndUpdate(filter, update);
-      // db.posts.findOneAndUpdate({_id:ObjectId("636bbcdb4a3b852574c4c3ad")},{$set:{"agrees":"1000"}})
-      console.log('thingis from findAndUpdate', post._id, post.agrees);
+      let post = await Post.findOneAndUpdate(filter, update);
     };
-    console.log('req.body.id from controler', req.body._id);
     findAndUpdate(req.body._id)
       .then(() => {
-        // console.log('res from findAndUpdate', res);
-        res.status(204);
-        // await console.log('res from controler', res);
+        res.status(204).json();
       })
       .catch((err) => {
-        // console.log(err);
-        // throw err;
+        throw err;
       });
-
-    // Post.findOneAndUpdate({ _id: `${req.id}` }, { $inc: { agrees: 1 } });
-
-    // const post = Post.findOne({ _id: req._id }, async (err) => {
-    //   console.log('request', req);
-    //   console.log('response', res);
-    //   if (err) {
-    //     throw err;
-    //   }
-    // });
-    // post.updateOne({ agrees: { $inc: 1 } });
   },
 };
 
