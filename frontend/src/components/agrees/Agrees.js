eb209de0af0ef66fa4b76b7bc981f7799fa1c0a3
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-09 17:31:59 +0000

    Remove console.logs and unnecessary comments

--- frontend/src/components/agrees/Agrees.js
+++ frontend/src/components/agrees/Agrees.js
@@ -2,9 +2,6 @@ const Agrees = ({ post_id, fetchPosts }) => {
   const token = window.localStorage.getItem('token');
 
   const addAgree = () => {
-    console.log('post id from Agrees: ', post_id);
-
-    // event.preventDefault();
     let response1 = fetch('/posts', {
       method: 'PATCH',
       headers: {
@@ -12,34 +9,15 @@ const Agrees = ({ post_id, fetchPosts }) => {
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({ _id: post_id }),
-    })
-      // .then((response) => console.log(response))
-      // .then((response) => response.json())
-      .then((responseJSON) => {
+    }).then((responseJSON) => {
+      if (responseJSON.status !== 204) {
         console.log(responseJSON);
-        if (responseJSON.status !== 204) {
-          console.log('there has been an error on the patch request');
-          console.log('response from agrees.js');
-        } else {
-          // let data = await response.json();
-          // window.localStorage.setItem('token', data.token);
-          fetchPosts();
-        }
-      });
+        console.log('There has been an error on the patch request');
+      } else {
+        fetchPosts();
+      }
+    });
   };
-
-  //   console.log('response.statuts: ', response.status);
-  //   console.log(response);
-
-  //   if (response.status !== 204) {
-  //     console.log('there has been an error on the patch request');
-  //     console.log('response from agrees.js');
-  //   } else {
-  //     // let data = await response.json();
-  //     // window.localStorage.setItem('token', data.token);
-  //     fetchPosts();
-  //   }
-  // };
 
   return (
     <button
