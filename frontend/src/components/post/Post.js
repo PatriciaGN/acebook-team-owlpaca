ddcc5ffde56f926ecdf8381592c311d51fb7c16c
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-09 17:07:43 +0000

    Add Agrees component to Post

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import Comments from '../comments/Comments'
-import Hates from '../hates/Hates'
-import "./Post.css";
+import Comments from '../comments/Comments';
+import Agrees from '../agrees/Agrees';
+import './Post.css';
+const moment = require('moment');
 
-const Post = ({ post }) => {
+const Post = ({ post, fetchPosts }) => {
+  const fullDate = new Date(post.created);
+  const timestamp = moment(fullDate).startOf('year').fromNow();
+
   return (
     <article data-cy="post" key={post._id}>
       <div class="header-container">
-        <img class="profile-image" src='https://iili.io/mVK9G2.png' alt='kyle' />
+        <img
+          class="profile-image"
+          src="https://iili.io/mVK9G2.png"
+          alt="kyle"
+        />
         <div class="name-and-time-container">
-          <div class="username">{"{ Name }"}</div>
-          <div class="post-time">{"{ Post time }"}</div>
+          <div class="username">{'{ Name }'}</div>
+          <div class="post-time">{timestamp}</div>
         </div>
       </div>
 
       <div class="message-container">
         <div class="message">{post.message}</div>
       </div>
-      
+
       <div class="post-image-container">
-        <img class="post-image" src='https://i.postimg.cc/T5vGJyXj/kyle.png' alt='kyle' />
+        <img
+          class="post-image"
+          src="https://i.postimg.cc/T5vGJyXj/kyle.png"
+          alt="kyle"
+        />
       </div>
-    
-      <Hates />
+
+      <div class="post-image-container">
+        <div class="Likes">{post.agrees}</div>
+      </div>
+
+      <Agrees post_id={post._id} fetchPosts={fetchPosts} />
       <Comments />
     </article>
-  )
-}
+  );
+};
 
 export default Post;
