df5a716721b5075ef4c26032e1c7f5d3092e64ac
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-11 11:20:38 +0000

    Remove unused import for app.css

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,29 +1,26 @@
-import './App.css';
-import LoginForm from '../auth/LoginForm'
-import SignUpForm from '../user/SignUpForm'
+import LoginForm from '../auth/LoginForm';
+import SignUpForm from '../user/SignUpForm';
 import React, { useState } from 'react';
-import Feed from '../feed/Feed'
-import Homepage from '../homepage/Homepage'
-import Navbar from './navbar'
-import {
-  useNavigate,
-  Routes,
-  Route,
-} from "react-router-dom";
+import Feed from '../feed/Feed';
+import Homepage from '../homepage/Homepage';
+import Navbar from './navbar';
+import { useNavigate, Routes, Route } from 'react-router-dom';
 
 const App = () => {
- 
-    return (
-      <>
-       <Navbar />
-        <Routes>
-          <Route path='/'  element={<Homepage navigate={ useNavigate() }/>}/>
-          <Route path='/posts'  element={<Feed navigate={ useNavigate() }/>}/>
-          <Route path='/login'  element={<LoginForm  navigate={ useNavigate() }/>}/>
-          <Route path='/signup' element={<SignUpForm navigate={ useNavigate() }/>}/>
-        </Routes>
-      </>
-    );
-}
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage navigate={useNavigate()} />} />
+        <Route path="/posts" element={<Feed navigate={useNavigate()} />} />
+        <Route path="/login" element={<LoginForm navigate={useNavigate()} />} />
+        <Route
+          path="/signup"
+          element={<SignUpForm navigate={useNavigate()} />}
+        />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
